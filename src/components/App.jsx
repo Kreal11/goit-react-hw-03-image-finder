2d92540dc39dc3b77972a430e7df2b7a9837abc2
Kreal11 <kirillshyrokov2@gmail.com>
2023-10-24 20:59:41 +0300

    did backend request for images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,40 @@
 import { Component } from 'react';
 // import axios from 'axios';
 import { ImageGallery } from './ImageGallery/ImageGallery';
-import { Searchbar } from './Searchbar/Searchbar';
+// import { Searchbar } from './Searchbar/Searchbar';
+import axios from 'axios';
 
 class App extends Component {
+  #KEY = '39794314-b7170df023ca4db44fdda06f6';
+  #BASE_URL = 'https://pixabay.com/api/';
+  #END_POINT = '';
+  per_page = 12;
+  page = 1;
+
+  state = {
+    images: [],
+    filter: '',
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(this.#BASE_URL, {
+        params: {
+          key: this.#KEY,
+          per_page: this.per_page,
+          page: this.page,
+        },
+      });
+      if (response.data.hits) {
+        this.setState({ images: response.data.hits });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   render() {
+    const { images } = this.state;
     return (
       <div
         style={{
@@ -15,9 +45,8 @@ class App extends Component {
           color: '#010101',
         }}
       >
-        {' '}
-        <Searchbar />
-        <ImageGallery></ImageGallery>
+        {/* <Searchbar /> */}
+        <ImageGallery images={images}></ImageGallery>
       </div>
     );
   }
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,11 @@
+import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
+
+export const ImageGallery = ({ images }) => {
+  return (
+    <div>
+      {images.map(image => (
+        <ImageGalleryItem key={image.id} {...image} />
+      ))}
+    </div>
+  );
+};
