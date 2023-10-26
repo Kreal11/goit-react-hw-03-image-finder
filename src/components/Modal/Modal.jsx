345b5e34022d2eeda97d5d7579e10f4df87e6679
Kreal11 <kirillshyrokov2@gmail.com>
2023-10-26 17:44:37 +0300

    added styles for modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,20 +1,36 @@
+import { toast } from 'react-toastify';
 import { StyledWrapperModal, StyledWrapperOverlay } from './Modal.styled';
 const { Component } = require('react');
 
 export class Modal extends Component {
+  handleKeyDown = e => {
+    if (e.key === 'Escape') {
+      this.props.close();
+      toast.info('Modal was closed by Escape');
+    }
+  };
+
   componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
     document.body.style.overflow = 'hidden';
   }
 
   componentWillUnmount() {
     document.body.style.overflow = 'visible';
+    document.removeEventListener('keydown', this.handleKeyDown);
   }
+
+  handleClickOut = ({ target, currentTarget }) => {
+    if (target === currentTarget) {
+      this.props.close();
+    }
+  };
 
   render() {
     return (
-      <StyledWrapperOverlay>
+      <StyledWrapperOverlay onClick={this.handleClickOut}>
         <StyledWrapperModal>
-          <button onClick={this.props.close}>Close window</button>
+          <button onClick={this.props.close}>Exit</button>
           {this.props.children}
         </StyledWrapperModal>
       </StyledWrapperOverlay>
--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -2,6 +2,15 @@ import styled from 'styled-components';
 
 export const StyledWrapperOverlay = styled.div`
   position: fixed;
+
+  inset: 0;
+  display: flex;
+  justify-content: center;
+  overflow-y: scroll;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(10px);
+  /* position: fixed;
   top: 0;
   left: 0;
   width: 100vw;
@@ -10,12 +19,38 @@ export const StyledWrapperOverlay = styled.div`
   flex-direction: column;
   /* justify-content: center; */
   /* align-items: center; */
-  background-color: rgba(0, 0, 0, 0.8);
-  z-index: 3;
+  /* background-color: rgba(0, 0, 0, 0.8);
+  z-index: 3;  */
 `;
 
 export const StyledWrapperModal = styled.div`
-  max-width: calc(100vw - 48px);
+  max-width: 900px;
+  max-height: 700px;
+  position: relative;
+  background-color: white;
+  padding: 20px;
+  overflow: hidden;
+  max-width: 1000px;
+  border-radius: 5px;
+  img {
+    width: 100%;
+    max-height: 500px;
+    object-fit: contain;
+    /* aspect-ratio: 16/9; */
+  }
+  button {
+    color: black;
+    font-weight: bold;
+    position: absolute;
+    top: 1px;
+    right: 1px;
+    background-color: transparent;
+    border: none;
+    font-size: 20px;
+    cursor: pointer;
+  }
+
+  /* max-width: calc(100vw - 48px);
   max-height: calc(100vh - 48px);
   overflow: hidden;
   position: relative;
@@ -24,15 +59,14 @@ export const StyledWrapperModal = styled.div`
   flex-direction: column;
   align-items: center;
   button {
-    margin-left: auto;
-    /* position: absolute;
-    right: 0;
-    top: 0; */
+    position: relative;
+    top: 30px;
+    left: 200px;
   }
   img {
     width: 100%;
     height: 100%;
     object-fit: contain;
     aspect-ratio: 16/9;
-  }
+  } */
 `;
